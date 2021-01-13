3fbd47ad96c63e3db141305ca95fbb9ab4564f96
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-01-12 23:14:06 -0500

    added auth-remember component

--- src/app/auth-form/auth-form.module.ts
+++ src/app/auth-form/auth-form.module.ts
@@ -3,11 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { AuthFormComponent } from './auth-form.component';
+import { AuthRememberComponent } from './auth-remember/auth-remember.component';
 
 
 @NgModule({
   declarations: [
-    AuthFormComponent
+    AuthFormComponent,
+    AuthRememberComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/auth-form/auth-remember/auth-remember.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-remember',
+  templateUrl: './auth-remember.component.html',
+  styleUrls: ['./auth-remember.component.css']
+})
+export class AuthRememberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
