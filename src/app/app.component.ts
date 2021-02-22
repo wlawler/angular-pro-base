ece51d7da6b8a5c43d4722a9727b66d02c8169e7
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-02-21 20:08:57 -0500

    set up for view encapsulation video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
-import { AfterContentInit, Component, ViewChild, TemplateRef, ViewContainerRef} from '@angular/core';
-import {AuthFormComponent } from './auth-form/auth-form.component';
-import { User } from './auth-form/auth-form.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl:'./app.component.html', 
-  styleUrls: ['./app.component.css']
-
-  
+  template: `
+    <div>
+      <example-one></example-one>
+      <example-two></example-two>
+      <example-three></example-three>
+    </div>
+  `
 })
-
 export class AppComponent {
- ctx = { 
-   $implicit: 'Wes Lawler', 
-   location: 'Louisville, KY'
- };
 }
