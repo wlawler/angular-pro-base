717e9d28dbc61db1e3fa96a8c0da7270432fe982
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-02-23 21:34:19 -0500

    set up for attribute directive video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,10 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  changeDetection: ChangeDetectionStrategy.Default,
-  templateUrl:'./app.component.html',
+  templateUrl: './app.component.html',
+   
+  
 })
 export class AppComponent {
-  user: any = {
-    name: 'Mark Hoppus',
-    age: 44,
-    location: 'California'
-  };
-
-  addProp() {
-    this.user.email = 'blink@blink-182.net';
-  }
-
-  changeName() {
-    this.user.name = 'Travis Barker';
-  }
-
-  changeUser() {
-    this.user = {
-      name: 'Tom Delonge',
-      age: 42,
-      location: 'California'
-    };
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ExampleOneComponent } from './one/one.component';
-import { ExampleTwoComponent } from './two/two.component';
+import { CreditCardDirective } from './credit-card/credit-card.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleOneComponent,
-    ExampleTwoComponent
+    CreditCardDirective
   ],
   imports: [
     BrowserModule
