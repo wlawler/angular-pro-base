187327c417de64a6826b09011ca4fea30db656a1
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-03-12 21:29:29 -0500

    finished remove form array video

--- src/app/stock-inventory/components/stock-products/stock-products/stock-products.component.ts
+++ src/app/stock-inventory/components/stock-products/stock-products/stock-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 import { FormGroup, FormArray } from '@angular/forms';
 
 @Component({
@@ -10,6 +10,12 @@ export class StockProductsComponent  {
 @Input()
 parent: FormGroup; 
 
+@Output()
+removed = new EventEmitter<any>();
+
+onRemove(group, index) {
+  this.removed.emit({ group, index});
+}
 
 get stocks(){
 return (this.parent.get('stock') as FormArray).controls;
--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -44,6 +44,11 @@ export class StockInventoryComponent {
     control.push(this.createStock(stock));
   }
 
+  removeStock({group, index }: { group: FormGroup, index: number}) {
+    const control = this.form.get('stock') as FormArray; 
+    control.removeAt(index);
+  }
+
   onSubmit(){
     console.log('Submit', this.form.value); 
   }
