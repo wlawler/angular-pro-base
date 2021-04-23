72d08dbc5f91001761ebf824ba4ff47dba2c8bfb
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-04-23 16:23:13 -0400

    finished custom control base video

--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -10,6 +10,7 @@ import { StockInventoryComponent } from './containers/stock-inventory/stock-inve
 import { StockBranchComponent } from './components/stock-branch/stock-branch/stock-branch.component';
 import { StockProductsComponent } from './components/stock-products/stock-products/stock-products.component';
 import { StockSelectorComponent } from './components/stock-selector/stock-selector/stock-selector.component';
+import { StockCounterComponent} from './components/stock-counter/stock-counter.component';
 import { StockInventoryService } from './services/stock-inventory.services';
 
 @NgModule({
@@ -18,6 +19,7 @@ import { StockInventoryService } from './services/stock-inventory.services';
     StockBranchComponent,
     StockProductsComponent,
     StockSelectorComponent,
+    StockCounterComponent
   ],
   imports: [
     CommonModule,
