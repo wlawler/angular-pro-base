cf3c10848c8962ae9aa1c2dac5bb4861fe826140
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-04-24 21:59:24 -0400

    finished keyboard event video

--- src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -36,6 +36,36 @@ writeValue(value) {
 
   value: number = 10; 
 
+  focus: boolean;
+
+  onKeyDown(event: KeyboardEvent) {
+   
+    const handlers = {
+        ArrowDown: () => this.decrement(), 
+        ArrowUp: () => this.increment()
+    };
+
+    if(handlers[event.code]) {
+       handlers[event.code](); 
+       event.preventDefault();
+       event.stopPropagation();
+    }
+    this.onTouch(); 
+  }
+
+  onBlur(event: FocusEvent){
+      this.focus = false;
+      event.preventDefault();
+      event.stopPropagation();
+      this.onTouch();
+  }
+  onFocus(event: FocusEvent){
+      this.focus = true; 
+      event.preventDefault();
+      event.stopPropagation(); 
+      this.onTouch(); 
+  }
+
 increment() {
   if ( this.value < this.max){  
   this.value = this.value + this.step; 
